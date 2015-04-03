2cc15fa15d02018b19c4e389d7c3018371cbc439
jiangli <liminghaoze@qq.com>
2015-04-03 14:34:05 +0800

    first commit

---
+++ routes/video.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+
+var video = require('../lib/index');
+
+
+router.post('/getmp4', function(req, res) {
+
+
+  console.log(req.body);
+
+  var url = req.body.url;
+
+  video(url, true,function (err,data) {
+
+    var retjson = {
+      code:0,
+      mag:''
+    };
+    if(err){
+      retjson.code = -1;
+      retjson.msg = err;
+      return res.send(retjson);
+    }
+    retjson.data = data;
+    res.send(retjson);
+
+  });
+
+
+
+
+});
+
+module.exports = router;
